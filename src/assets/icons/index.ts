4061c9f5200153ed0a4d96d29e0975ee488a10b2
tonbots <kartono.saleh@gmail.com>
2023-08-05 18:01:24 +0700

    Menambahkan icons di folder assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,45 @@
+import home from "./home.svg";
+import user from "./user.svg";
+import product from "./product.svg";
+import order from "./order.svg";
+import post2 from "./post2.svg";
+import element from "./element.svg";
+import note from "./note.svg";
+import form from "./form.svg";
+import calender from "./calendar.svg";
+import setting from "./setting.svg";
+import backup from "./backup.svg";
+import chart from "./chart.svg";
+import log from "./log.svg";
+
+import logo from "./logo.svg";
+import search from "./search.svg";
+import app from "./app.svg";
+import expand from "./expand.svg";
+import notif from "./notifications.svg";
+import settings from "./settings.svg";
+
+import kartono from "./kartono.png";
+
+export {
+  home,
+  user,
+  product,
+  order,
+  post2,
+  element,
+  note,
+  form,
+  calender,
+  setting,
+  backup,
+  chart,
+  log,
+  logo,
+  search,
+  app,
+  expand,
+  notif,
+  settings,
+  kartono,
+};
