a41e54553ffee73c7fd443823b33b20ce13d5c89
tonbots <kartono.saleh@gmail.com>
2023-08-06 16:21:02 +0700

    Menambahkan dummy data

---
+++ src/constants/Dummy.ts
@@ -0,0 +1,130 @@
+import {
+  conversionIcon,
+  productIcon,
+  revenueIcon,
+  userIcon,
+} from "../assets/icons";
+
+export const topDealUsers = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/8405873/pexels-photo-8405873.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    username: "Meow Juned",
+    email: "meow@gmail.com",
+    amount: "3.668",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Nengsih Ayu",
+    email: "nengsih@gmail.com",
+    amount: "1.200",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Samsul Bahri",
+    email: "samsul@gmail.com",
+    amount: "2.800",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Nur Amelia",
+    email: "amelia@gmail.com",
+    amount: "4.900",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Juan Bores",
+    email: "juan@gmail.com",
+    amount: "1.323",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Silvia Agustin",
+    email: "silvia@gmail.com",
+    amount: "2.999",
+  },
+  {
+    id: 7,
+    img: "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    username: "Deddy Wijaya",
+    email: "wijaya@gmail.com",
+    amount: "4.100",
+  },
+];
+
+export const chartBoxUser = {
+  color: "#8884d8",
+  icon: userIcon,
+  title: "Total Users",
+  number: "11.238",
+  dataKey: "users",
+  percentage: 45,
+  chartData: [
+    { name: "Sun", users: 400 },
+    { name: "Mon", users: 600 },
+    { name: "Tue", users: 500 },
+    { name: "Wed", users: 700 },
+    { name: "Thu", users: 400 },
+    { name: "Fri", users: 500 },
+    { name: "Sat", users: 450 },
+  ],
+};
+
+export const chartBoxProduct = {
+  color: "skyblue",
+  icon: productIcon,
+  title: "Total Products",
+  number: "238",
+  dataKey: "products",
+  percentage: 21,
+  chartData: [
+    { name: "Sun", products: 400 },
+    { name: "Mon", products: 600 },
+    { name: "Tue", products: 500 },
+    { name: "Wed", products: 700 },
+    { name: "Thu", products: 400 },
+    { name: "Fri", products: 500 },
+    { name: "Sat", products: 450 },
+  ],
+};
+
+export const chartBoxRevenue = {
+  color: "teal",
+  icon: revenueIcon,
+  title: "Total Revenue",
+  number: "$56.432",
+  dataKey: "revenue",
+  percentage: -12,
+  chartData: [
+    { name: "Sun", revenue: 400 },
+    { name: "Mon", revenue: 600 },
+    { name: "Tue", revenue: 500 },
+    { name: "Wed", revenue: 700 },
+    { name: "Thu", revenue: 400 },
+    { name: "Fri", revenue: 500 },
+    { name: "Sat", revenue: 450 },
+  ],
+};
+
+export const chartBoxConversion = {
+  color: "gold",
+  icon: conversionIcon,
+  title: "Total Ratio",
+  number: "2.6",
+  dataKey: "ratio",
+  percentage: 12,
+  chartData: [
+    { name: "Sun", ratio: 400 },
+    { name: "Mon", ratio: 600 },
+    { name: "Tue", ratio: 500 },
+    { name: "Wed", ratio: 700 },
+    { name: "Thu", ratio: 400 },
+    { name: "Fri", ratio: 500 },
+    { name: "Sat", ratio: 450 },
+  ],
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+import { menu } from "./DataConstants";
+import { topDealUsers } from "./Dummy";
+import {
+  chartBoxUser,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxConversion,
+} from "./Dummy";
+
+export {
+  menu,
+  topDealUsers,
+  chartBoxUser,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxConversion,
+};
