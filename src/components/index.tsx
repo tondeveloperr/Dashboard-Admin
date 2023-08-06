3cbaaf2cf3d3ee4997f9c276d6c2b8c5c0bfb7e7
tonbots <kartono.saleh@gmail.com>
2023-08-06 17:22:43 +0700

    Membuat barChartBox component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,6 @@ import Menu from "./menu/Menu";
 import Footer from "./footer/Footer";
 import TopBox from "./topBox/TopBox";
 import ChartBox from "./chartBox/ChartBox";
+import BarChartBox from "./barChartBox/BarChartBox";
 
-export { Navbar, Menu, Footer, TopBox, ChartBox };
+export { Navbar, Menu, Footer, TopBox, ChartBox, BarChartBox };
