9c22da9fd225e1ede7bc90e4133411957250a513
tonbots <kartono.saleh@gmail.com>
2023-08-06 16:24:15 +0700

    membuat component menu

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,7 @@
 import Navbar from "./navbar/Navbar";
 import Menu from "./menu/Menu";
 import Footer from "./footer/Footer";
+import TopBox from "./topBox/TopBox";
+import ChartBox from "./chartBox/ChartBox";
 
-export { Navbar, Menu, Footer };
+export { Navbar, Menu, Footer, TopBox, ChartBox };
