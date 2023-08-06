7e780de2b6f6a5030620642444ed9b9d1b82d983
tonbots <kartono.saleh@gmail.com>
2023-08-06 16:26:46 +0700

    styling untuk component footer

---
+++ src/components/chartBox/ChartBox.tsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+import "./chartbox.scss";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
+
+type Props = {
+  color: string;
+  icon: string;
+  title: string;
+  dataKey: string;
+  number: number | string;
+  percentage: number;
+  chartData: object[];
+};
+
+const ChartBox = (props: Props) => {
+  return (
+    <div className="chartbox">
+      <div className="box-info">
+        <div className="title">
+          <img src={props.icon} alt="icon-user" />
+          <span>{props.title}</span>
+        </div>
+        <h1>{props.number}</h1>
+        <Link to="/" className="view" style={{ color: props.color }}>
+          View All
+        </Link>
+      </div>
+      <div className="chart-info">
+        <div className="chart">
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 70 }}
+              />
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span
+            className="percentage"
+            style={{ color: props.percentage < 0 ? "tomato" : "limegreen" }}
+          >
+            {props.percentage}%
+          </span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartBox;
