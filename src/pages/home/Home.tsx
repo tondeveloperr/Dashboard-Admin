57d0a4a1550b5f58f38c7c8b45e03b64dbc5f975
tonbots <kartono.saleh@gmail.com>
2023-08-13 13:49:03 +0700

    Menerapkan komponen charts di halaman home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,52 @@
+import {
+  BarChartBox,
+  BigChartBox,
+  ChartBox,
+  PieChartBox,
+  TopBox,
+} from "../../components";
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../constants";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="box box-1">
+        <TopBox />
+      </div>
+      <div className="box box-2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box-3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box-4">
+        <PieChartBox />
+      </div>
+      <div className="box box-5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box-6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box-7">
+        <BigChartBox />
+      </div>
+      <div className="box box-8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box-9">
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
