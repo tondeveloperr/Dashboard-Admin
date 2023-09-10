efff35a708d4eca7e403344a75c36909991160f0
tonbots <kartono.saleh@gmail.com>
2023-09-10 20:17:40 +0700

    Menambahkan dummy data products

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,6 +8,7 @@ import {
   barChartBoxRevenue,
   barChartBoxVisit,
   userRows,
+  products,
 } from "./Dummy";
 
 export {
@@ -20,4 +21,5 @@ export {
   barChartBoxRevenue,
   barChartBoxVisit,
   userRows,
+  products,
 };
