2dd697550027e4f212e15048ef0ccd7c890868e0
tondeveloperr <kartono.saleh@gmail.com>
2023-09-11 16:13:23 +0700

    update komponen footer, untuk menambahkan label

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,19 @@ import "./footer.scss";
 const Footer = () => {
   return (
     <div className="footer">
-      <span>Dashboard</span>
-      <span>TonDeveloper</span>
+      {/* <hr /> */}
+      <p>
+        &copy;2023{" "}
+        <a
+          href="https://kartono-portfolio.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="label"
+        >
+          TonDeveloper
+        </a>
+        . Made with ❤️ and React.js.
+      </p>
     </div>
   );
 };
